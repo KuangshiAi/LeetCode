5870e82f46b17fb703df3a96f26f04e48bc8f233
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2024-09-08 21:24:14 -0400

    LC-group-anagrams [Runtime: N/A; Memory: N/A]

---
+++ sorting/group-anagrams.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        res = {}
+        for word in strs:
+            char_set = frozenset(list(word))
+            if char_set not in res.keys():
+                res[char_set] = [word]
+            else:
+                res[char_set].append(word)
+            
+        return res.values()      
