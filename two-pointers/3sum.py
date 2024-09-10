05fcfa90ef555a4f5680ed3c66f83211a2ec3047
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2024-09-10 14:08:18 -0400

    LC-3sum [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/3sum.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums = sorted(nums)
+        i = 0
+        j = i + 1
+        k = len(nums) - 1
+        res = []
+        while True:
+            while j < k:
+                if nums[i] + nums[j] + nums[k] == 0:
+                    res.append([nums[i], nums[j], nums[k]])
+                    break
+                elif nums[i] + nums[j] + nums[k] > 0:
+                    k -= 1
+                else:
+                    j += 1
+            
+            i += 1
+            j = i +1
+            k = len(nums) - 1
+            if i == len(nums) - 2:
+                break
+        return res
+        
