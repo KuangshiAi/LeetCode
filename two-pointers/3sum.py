08dba83767a983131e0ee35c662f46a0ea37ddba
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2024-09-10 14:14:18 -0400

    LC-3sum [Runtime: 5910 ms; Memory: 14.9 MB]

--- two-pointers/3sum.py
+++ two-pointers/3sum.py
@@ -14,7 +14,7 @@ class Solution(object):
                 if nums[i] + nums[j] + nums[k] == 0:
                     if [nums[i], nums[j], nums[k]] not in res:
                         res.append([nums[i], nums[j], nums[k]])
-                    break
+                    k -= 1
                 elif nums[i] + nums[j] + nums[k] > 0:
                     k -= 1
                 else:
