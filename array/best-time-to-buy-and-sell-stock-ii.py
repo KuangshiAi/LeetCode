d7749fbef3777bc372854e78b14fbafbf03eb1d0
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2024-09-12 20:23:40 -0400

    LC-best-time-to-buy-and-sell-stock-ii [Runtime: 60 ms; Memory: 12.9 MB]

---
+++ array/best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        res = 0
+        buy_price = prices[0]
+        for i in range(len(prices)):
+            if prices[i] > buy_price:
+                res += prices[i] - buy_price
+            buy_price = prices[i]
+        return res
