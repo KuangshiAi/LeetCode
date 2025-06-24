6c23c728160607f27e0a2d29147f7ec8b3da3ce0
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-06-24 16:54:52 -0400

    LC-remove-duplicates-from-sorted-array-ii [Runtime: 78 ms; Memory: 15.3 MB]

--- two-pointers/remove-duplicates-from-sorted-array-ii.py
+++ two-pointers/remove-duplicates-from-sorted-array-ii.py
@@ -4,23 +4,29 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums)<3:
-            return len(nums)
+        # if len(nums)<3:
+        #     return len(nums)
         slow_pointer=0
         fast_pointer=0
-        while fast_pointer<len(nums)-2:
-            if nums[fast_pointer]<nums[fast_pointer+2]:
+        while fast_pointer<len(nums):
+            if fast_pointer+2<len(nums):
+                if nums[fast_pointer]<nums[fast_pointer+2]:
+                    nums[slow_pointer]=nums[fast_pointer]
+                    slow_pointer+=1
+                fast_pointer+=1
+            else:
                 nums[slow_pointer]=nums[fast_pointer]
                 slow_pointer+=1
-            fast_pointer+=1
-        if len(nums)==3:
-            nums[slow_pointer]=nums[fast_pointer]
-            slow_pointer+=1
-        else:
-            nums[slow_pointer]=nums[fast_pointer]
-            slow_pointer+=1
-            nums[slow_pointer]=nums[fast_pointer+1]
-            slow_pointer+=1
+                fast_pointer+=1
+
+        # if len(nums)==3:
+        #     nums[slow_pointer]=nums[fast_pointer]
+        #     slow_pointer+=1
+        # else:
+        #     nums[slow_pointer]=nums[fast_pointer]
+        #     slow_pointer+=1
+        #     nums[slow_pointer]=nums[fast_pointer+1]
+        #     slow_pointer+=1
         return slow_pointer
 
 
