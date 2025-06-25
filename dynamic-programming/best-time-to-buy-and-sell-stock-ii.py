763406924a0b37f4f112ba3913df8626d54d9161
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-06-25 10:59:38 -0400

    LC-best-time-to-buy-and-sell-stock-ii [Runtime: 0 ms; Memory: 13.7 MB]

--- dynamic-programming/best-time-to-buy-and-sell-stock-ii.py
+++ dynamic-programming/best-time-to-buy-and-sell-stock-ii.py
@@ -4,10 +4,10 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        res = 0
-        buy_price = prices[0]
+        res=0
+        buy_price=prices[0]
         for i in range(len(prices)):
-            if prices[i] > buy_price:
-                res += prices[i] - buy_price
-            buy_price = prices[i]
+            if prices[i]>buy_price:
+                res += prices[i]-buy_price
+            buy_price=prices[i]
         return res
