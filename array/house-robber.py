330449ebe7fa1d9b168dff2066d31be84c8b3595
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-06-26 13:08:24 -0400

    LC-house-robber [Runtime: N/A; Memory: N/A]

---
+++ array/house-robber.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        bp = [0]*n
+        bp[0] = nums[0]
+        bp[1] = min(nums[0], nums[1])
+        for i in range(2, n):
+            bp[i] = max(bp[i-1], bp[i-2]+nums[i])
+        return bp[n-1]
+        
