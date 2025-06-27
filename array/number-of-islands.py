4c8a82cea0614de78f5f8d795726862730c33f85
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-06-27 17:33:25 -0400

    LC-number-of-islands [Runtime: N/A; Memory: N/A]

--- array/number-of-islands.py
+++ array/number-of-islands.py
@@ -9,33 +9,32 @@ class Solution(object):
         cols = len(grid[0])
         island = 0
         visited = set()
-        # def bfs(r,c):
-        #     q = deque()
-        #     visited.add((r,c))
-        #     q.append((r,c))
-        #     directions = [[1,0], [-1,0], [0,1], [0,-1]]
-        #     while q:
-        #         row, col = q.popleft()
-        #         for dr, dc in directions:
-        #             r, c = row + dr, col + dc
-        #             if 0<=r<rows and 0<=c<cols and grid[r][c]=="1" and (r,c) not in visited:
-        #                 q.append((r,c))
-        #                 visited.add((r,c))
-
-        def dfs(r,c):
+        def bfs(r,c):
+            q = deque()
+            q.append((r,c))
             directions = [[1,0], [-1,0], [0,1], [0,-1]]
-            if (r,c) not in visited and grid[r][c] == "1":
-                visited.add((r,c))
-            for dr, dc in directions:
-                r_, c_ = r + dr, c + dc
-                if 0<=r_<rows and 0<=c_<cols and grid[r_][c_]=="1" and (r_,c_) not in visited:
-                    dfs(r_,c_)
+            while q:
+                row, col = q.popleft()
+                visited.add((row, col))
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc
+                    if 0<=r<rows and 0<=c<cols and grid[r][c]=="1" and (r,c) not in visited:
+                        q.append((r,c))
+
+        # def dfs(r,c):
+        #     directions = [[1,0], [-1,0], [0,1], [0,-1]]
+        #     if (r,c) not in visited and grid[r][c] == "1":
+        #         visited.add((r,c))
+        #     for dr, dc in directions:
+        #         r_, c_ = r + dr, c + dc
+        #         if 0<=r_<rows and 0<=c_<cols and grid[r_][c_]=="1" and (r_,c_) not in visited:
+        #             dfs(r_,c_)
         
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == "1" and (i,j) not in visited:
                     island += 1
-                    dfs(i,j)
+                    bfs(i,j)
         return island
 
         
