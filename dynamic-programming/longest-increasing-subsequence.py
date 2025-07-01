c704011a88421803ec29bee476faa5beb35ebc32
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-07-01 12:34:13 -0400

    LC-longest-increasing-subsequence [Runtime: 10 ms; Memory: 12.7 MB]

--- dynamic-programming/longest-increasing-subsequence.py
+++ dynamic-programming/longest-increasing-subsequence.py
@@ -4,11 +4,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        dp = [1] * len(nums)
-        for i in range(len(nums)):
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    dp[i] = max(dp[j]+1, dp[i])
-        return max(dp)
+        # dp = [1] * len(nums)
+        # for i in range(len(nums)):
+        #     for j in range(i):
+        #         if nums[i] > nums[j]:
+        #             dp[i] = max(dp[j]+1, dp[i])
+        # return max(dp)
+
+        tails = [nums[0]]
+        for k in range(1, len(nums)):
+            i, j =0, len(tails)
+            while i<j:
+                m = (i+j)//2
+                if tails[m] < nums[k]:
+                    i = m+1
+                else:
+                    j = m
+            if i == len(tails):
+                tails.append(nums[k])
+            else:
+                tails[i] = nums[k]
+            
+        return len(tails)
         
         
