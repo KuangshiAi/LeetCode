eb3b51546dc0c532238f8158e1bb26a2c00090c3
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-07-01 11:48:57 -0400

    LC-maximum-subarray [Runtime: 49 ms; Memory: 24.3 MB]

--- array/maximum-subarray.py
+++ array/maximum-subarray.py
@@ -4,15 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        self.max = nums[0]
-        slow = fast = 0
-        cur_sum = 0
-        for fast in range(len(nums)):
-            if cur_sum < 0:
-                slow = fast
-                cur_sum = nums[slow]
+        # self.max = nums[0]
+        # slow = fast = 0
+        # cur_sum = 0
+        # for fast in range(len(nums)):
+        #     if cur_sum < 0:
+        #         slow = fast
+        #         cur_sum = nums[slow]
+        #     else:
+        #         cur_sum += nums[fast]
+        #     if cur_sum > self.max:
+        #         self.max = cur_sum
+        # return self.max
+
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        for i in range(1, len(nums)):
+            if dp[i-1] <= 0:
+                dp[i] = nums[i]
             else:
-                cur_sum += nums[fast]
-            if cur_sum > self.max:
-                self.max = cur_sum
-        return self.max
+                dp[i] = dp[i-1] + nums[i]
+        return max(dp)
