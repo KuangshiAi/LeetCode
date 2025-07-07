dd6d209bc712288cdce2b2cfa95c5eadc81f1a2a
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-07-07 15:18:08 -0400

    LC-best-time-to-buy-and-sell-stock-ii [Runtime: 4 ms; Memory: 13.6 MB]

--- greedy/best-time-to-buy-and-sell-stock-ii.py
+++ greedy/best-time-to-buy-and-sell-stock-ii.py
@@ -4,10 +4,8 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        res=0
-        buy_price=prices[0]
-        for i in range(len(prices)):
-            if prices[i]>buy_price:
-                res += prices[i]-buy_price
-            buy_price=prices[i]
+        res = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                res += prices[i] - prices[i-1]
         return res
