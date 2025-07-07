96ebcb66fa2e0c30780a701153b9c048e9dc860a
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-07-07 15:55:41 -0400

    LC-longest-substring-without-repeating-characters [Runtime: 20 ms; Memory: 13.3 MB]

--- string/longest-substring-without-repeating-characters.py
+++ string/longest-substring-without-repeating-characters.py
@@ -4,13 +4,28 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        res = 0
-        dic = {} # the last place where s[i] appears
+        # res = 0
+        # dic = {} # the last place where s[i] appears
+        # i = -1
+        # for j in range(len(s)):
+        #     if s[j] in dic:
+        #         i = max(i, dic[s[j]])
+        #     dic[s[j]] = j
+        #     res = max(res, j-i)
+        # return res
+
+        # dp[i]: the length of the longest non-reapting substring ends with s[i]
         i = -1
+        res = 0
+        dp = 0
+        dic = {}
         for j in range(len(s)):
-            if s[j] in dic:
-                i = max(i, dic[s[j]])
+            i = dic.get(s[j], -1)
             dic[s[j]] = j
-            res = max(res, j-i)
+            if dp < j-i:
+                dp += 1
+            else:
+                dp = j-i
+            res = max(res, dp)
         return res
         
