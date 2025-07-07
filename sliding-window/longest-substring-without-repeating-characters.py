7d5472b781ae2209120e70d4c8c5f9b9fc93cc1f
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-07-07 15:44:29 -0400

    LC-longest-substring-without-repeating-characters [Runtime: N/A; Memory: N/A]

---
+++ sliding-window/longest-substring-without-repeating-characters.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        res = 0
+        dic = {} # the last place where s[i] appears
+        i = 0
+        for j in range(len(s)):
+            if s[j] in dic:
+                i = max(i, dic[s[j]])
+            dic[s[j]] = j
+            res = max(res, j-i)
+        return res
+        
