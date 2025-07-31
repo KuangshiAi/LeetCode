3f6d56cb6dae1a17937caa28dec51eaafcc718df
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-07-31 11:34:12 -0400

    LC-combinations [Runtime: 453 ms; Memory: 132 MB]

---
+++ backtracking/combinations.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        res = []
+        def backtrack(comb, start, k_left):
+            if k_left == 0:
+                res.append(comb[:])
+            for i in range(start, n+1):
+                comb.append(i)
+                backtrack(comb, i+1, k_left - 1)
+                comb.pop()
+        backtrack([], 1, k)
+        return res
+        
