29734a478328c17f444944276ecfe0f0e214dcec
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-07-30 23:52:04 -0400

    LC-minimum-operations-to-make-character-frequencies-equal [Runtime: 2281 ms; Memory: 169.7 MB]

--- string/minimum-operations-to-make-character-frequencies-equal.py
+++ string/minimum-operations-to-make-character-frequencies-equal.py
@@ -1,35 +1,34 @@
 class Solution(object):
     def makeStringGood(self, s):
-        count = [0] * 26
-        for c in s:
-            count[ord(c) - ord("a")] += 1
+        """
+        :type s: str
+        :rtype: int
+        """
+        freq = [0]*26
+        for char in s:
+            freq[ord(char)-ord('a')] += 1
 
         memo = {}
-
         def dp(i, deleted, target):
             key = (i, deleted, target)
             if key in memo:
                 return memo[key]
-
             if i == 26:
                 return 0
-
-            x = count[i]
-            if x == target or x == 0:
-                res = dp(i + 1, 0, target)
+            x = freq[i]
+            if x == target:
+                res = dp(i+1, 0, target)
             elif x > target:
-                res = dp(i + 1, x - target, target) + x - target
+                res = dp(i+1, x-target, target) + x-target
             else:
-                need = target - x
-                insert = dp(i + 1, 0, target) + need
-                delete = dp(i + 1, x, target) + x
-                change = dp(i + 1, 0, target) + need - min(deleted, need)
-                res = min(insert, delete, change)
-
+                insert = dp(i+1, 0, target) + target - x
+                delete = dp(i+1, x, target) + x
+                change = dp(i+1, 0, target) + target - x - min(deleted, target - x)
+                res = min(insert,delete,change)
             memo[key] = res
             return res
-
+        
         mini = float('inf')
-        for target in range(max(count) + 1):
-            mini = min(mini, dp(0, 0, target))
+        for target in range(max(freq)+1):
+            mini = min(mini, dp(0,0,target))
         return mini
