9a86abca526096bfe0ef751cbda3c7c3e8076603
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-08-06 10:05:59 -0700

    LC-number-of-islands [Runtime: 263 ms; Memory: 34.3 MB]

--- matrix/number-of-islands.py
+++ matrix/number-of-islands.py
@@ -9,24 +9,35 @@ class Solution(object):
         cols = len(grid[0])
         visited = set()
         directions = [[1,0],[-1,0],[0,1],[0,-1]]
-        def bfs(i,j):
-            queue = deque()
-            queue.append((i,j))
-            visited.add((i,j))
-            while queue:
-                cur_row, cur_col = queue.popleft()
-                for dr,dc in directions:
-                    row = cur_row + dr
-                    col = cur_col + dc
-                    if 0<=row<rows and 0<=col<cols and grid[row][col]=="1" and (row,col) not in visited:
-                        queue.append((row,col))
-                        visited.add((row,col))
+        # def bfs(i,j):
+        #     queue = deque()
+        #     queue.append((i,j))
+        #     visited.add((i,j))
+        #     while queue:
+        #         cur_row, cur_col = queue.popleft()
+        #         for dr,dc in directions:
+        #             row = cur_row + dr
+        #             col = cur_col + dc
+        #             if 0<=row<rows and 0<=col<cols and grid[row][col]=="1" and (row,col) not in visited:
+        #                 queue.append((row,col))
+        #                 visited.add((row,col))
+        def dfs(i,j):
+            if (i,j) in visited:
+                return
+            if (i,j) not in visited and grid[i][j]=="1":
+                visited.add((i,j))
+            for dr,dc in directions:
+                row = i + dr
+                col = j + dc
+                if 0<=row<rows and 0<=col<cols and grid[row][col]=="1" and (row,col) not in visited:
+                    dfs(row, col)
+
         island_num = 0
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == "1" and (i,j) not in visited:
                     island_num += 1
-                    bfs(i,j)
+                    dfs(i,j)
         return island_num
         
         
