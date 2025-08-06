adc86ebd9ce0f14bc1dfbb679546eb328a68b612
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-08-06 09:59:08 -0700

    LC-number-of-islands [Runtime: 240 ms; Memory: 31.5 MB]

--- array/number-of-islands.py
+++ array/number-of-islands.py
@@ -7,36 +7,26 @@ class Solution(object):
         """
         rows = len(grid)
         cols = len(grid[0])
-        island = 0
         visited = set()
-        def bfs(r,c):
-            q = deque()
-            q.append((r,c))
-            visited.add((r,c))
-            directions = [[1,0], [-1,0], [0,1], [0,-1]]
-            while q:
-                row, col = q.popleft()
-                for dr, dc in directions:
-                    r, c = row + dr, col + dc
-                    if 0<=r<rows and 0<=c<cols and grid[r][c]=="1" and (r,c) not in visited:
-                        q.append((r,c))
-                        visited.add((r,c))
-
-
-        # def dfs(r,c):
-        #     directions = [[1,0], [-1,0], [0,1], [0,-1]]
-        #     if (r,c) not in visited and grid[r][c] == "1":
-        #         visited.add((r,c))
-        #     for dr, dc in directions:
-        #         r_, c_ = r + dr, c + dc
-        #         if 0<=r_<rows and 0<=c_<cols and grid[r_][c_]=="1" and (r_,c_) not in visited:
-        #             dfs(r_,c_)
-        
+        directions = [[1,0],[-1,0],[0,1],[0,-1]]
+        def bfs(i,j):
+            queue = deque()
+            queue.append((i,j))
+            visited.add((i,j))
+            while queue:
+                cur_row, cur_col = queue.popleft()
+                for dr,dc in directions:
+                    row = cur_row + dr
+                    col = cur_col + dc
+                    if 0<=row<rows and 0<=col<cols and grid[row][col]=="1" and (row,col) not in visited:
+                        queue.append((row,col))
+                        visited.add((row,col))
+        island_num = 0
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == "1" and (i,j) not in visited:
-                    island += 1
+                    island_num += 1
                     bfs(i,j)
-        return island
-
+        return island_num
+        
         
