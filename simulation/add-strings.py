adda77a714f949619f881963ee38a89cba9745aa
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-08-10 22:56:44 -0400

    LC-add-strings [Runtime: 7 ms; Memory: 12.6 MB]

--- simulation/add-strings.py
+++ simulation/add-strings.py
@@ -5,16 +5,17 @@ class Solution(object):
         :type num2: str
         :rtype: str
         """
-        result = []
         carry = 0
-        i, j =len(num1)-1, len(num2)-1
-        while i>=0 or j>=0 or carry:
+        i, j = len(num1)-1, len(num2)-1
+        res = []
+        while i >=0 or j>=0 or carry >0:
             digit1 = ord(num1[i]) - ord('0') if i>=0 else 0
-            digit2 = ord(num2[j]) - ord('0') if j>=0 else 0
-            total = digit1+digit2+carry
+            digit2 = ord(num2[j]) - ord('0')  if j>=0 else 0
+            total = digit1 + digit2 + carry
+            res.append(chr(total % 10 + ord('0')))
             carry = total // 10
-            result.append(chr(total%10 + ord('0')))
             i -= 1
             j -= 1
-        return "".join(result[::-1])
+        return "".join(res[::-1])
+
         
