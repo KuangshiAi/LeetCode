becb4005495b503a73aac59af66d2c4043742185
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-08-16 18:48:47 -0400

    LC-jump-game-ii [Runtime: N/A; Memory: N/A]

---
+++ greedy/jump-game-ii.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dp = [0] * len(nums) # dp[i]: the minimal jumps needed from i to the end
+        for i in range(len(nums)-2, -1, -1):
+            min_jump = float('inf')
+            for j in range(i+1, i+nums[i]+1):
+                min_jump = min(min_jump, dp[j])
+            dp[i] = min_jump + 1
+        return dp[0]
+        
