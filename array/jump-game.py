c1514bce53c3a3ca8fcd2ce08d786ce4f04b93e9
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-08-16 18:37:40 -0400

    LC-jump-game [Runtime: 23 ms; Memory: 13.5 MB]

--- array/jump-game.py
+++ array/jump-game.py
@@ -8,9 +8,7 @@ class Solution(object):
         for i in range(len(nums)-2, -1, -1):
             if nums[i] + i >= goal:
                 goal = i
-        if goal == 0:
+        if goal <= 0:
             return True
         else:
             return False
-            
-        
