8c6dcfbc6e12036c7af6eba91f5d4df22c14f0b5
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-08-16 18:18:32 -0400

    LC-number-of-sub-arrays-with-odd-sum [Runtime: 195 ms; Memory: 18.3 MB]

--- math/number-of-sub-arrays-with-odd-sum.py
+++ math/number-of-sub-arrays-with-odd-sum.py
@@ -5,18 +5,34 @@ class Solution(object):
         :rtype: int
         """
         MOD = 10**9 + 7
-        ans = 0
-        prefix = 0
-        even_count = 1
-        odd_count = 0
-        for num in arr:
-            prefix += num
-            if prefix % 2 == 0:
-                ans += odd_count
-                even_count += 1
+        # ans = 0
+        # prefix = 0
+        # even_count = 1
+        # odd_count = 0
+        # for num in arr:
+        #     prefix += num
+        #     if prefix % 2 == 0:
+        #         ans += odd_count
+        #         even_count += 1
+        #     else:
+        #         ans += even_count
+        #         odd_count += 1
+        #     ans %= MOD            
+        # return ans
+        dp = [[0,0], [0,0]]
+        counter = 0
+        for i in range(len(arr)):
+            if i % 2 == 0:
+                idx = 0
             else:
-                ans += even_count
-                odd_count += 1
-            ans %= MOD            
-        return ans
+                idx = 1
+            if arr[i] % 2 == 0:
+                dp[0][idx] = dp[0][1-idx] + 1 # as [arr[i]] also counts
+                dp[1][idx] = dp[1][1-idx]
+            else:
+                dp[0][idx] = dp[1][1-idx]
+                dp[1][idx] = dp[0][1-idx] + 1
+            counter += dp[1][idx]
+            counter %= MOD
+        return counter
         
