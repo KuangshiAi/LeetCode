837dd3650cdeeff9bbdc149e39a5928fb7aa9370
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-08-16 17:59:35 -0400

    LC-number-of-sub-arrays-with-odd-sum [Runtime: 85 ms; Memory: 15.6 MB]

--- prefix-sum/number-of-sub-arrays-with-odd-sum.py
+++ prefix-sum/number-of-sub-arrays-with-odd-sum.py
@@ -4,14 +4,19 @@ class Solution(object):
         :type arr: List[int]
         :rtype: int
         """
+        MOD = 10**9 + 7
         ans = 0
-        hashmap = {0:1}
         prefix = 0
+        even_count = 1
+        odd_count = 0
         for num in arr:
             prefix += num
-            for key, value in hashmap.items():
-                if (prefix - key) % 2 == 1:
-                    ans += value
-            hashmap[prefix] = hashmap.get(prefix, 0) + 1
+            if prefix % 2 == 0:
+                ans += odd_count
+                even_count += 1
+            else:
+                ans += even_count
+                odd_count += 1
+            ans %= MOD            
         return ans
         
