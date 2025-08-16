01db9324315ade8cbfb4a62ad4cd666bad79f4be
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-08-15 22:44:07 -0400

    LC-h-index [Runtime: 0 ms; Memory: 12.4 MB]

--- counting-sort/h-index.py
+++ counting-sort/h-index.py
@@ -15,7 +15,7 @@ class Solution(object):
             else:
                 return False
         while left < right:
-            mid = (left + right) // 2
+            mid = (left + right + 1) // 2
             if check_h_index(mid, citations):
                 left = mid
             else:
