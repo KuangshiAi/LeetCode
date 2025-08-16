97968e951c36e7e0911a46b83ec44f2994728a7f
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-08-16 18:49:30 -0400

    LC-jump-game-ii [Runtime: 3155 ms; Memory: 13.4 MB]

--- array/jump-game-ii.py
+++ array/jump-game-ii.py
@@ -7,7 +7,7 @@ class Solution(object):
         dp = [0] * len(nums) # dp[i]: the minimal jumps needed from i to the end
         for i in range(len(nums)-2, -1, -1):
             min_jump = float('inf')
-            for j in range(i+1, i+nums[i]+1):
+            for j in range(i+1, min(i+nums[i]+1, len(nums))):
                 min_jump = min(min_jump, dp[j])
             dp[i] = min_jump + 1
         return dp[0]
