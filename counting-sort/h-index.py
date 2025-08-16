aa0a93f9cd0232f4b2f3a8c7f2185a6e06656a86
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-08-15 23:01:11 -0400

    LC-h-index [Runtime: 0 ms; Memory: 12.6 MB]

--- counting-sort/h-index.py
+++ counting-sort/h-index.py
@@ -4,21 +4,29 @@ class Solution(object):
         :type citations: List[int]
         :rtype: int
         """
-        left, right = min(min(citations), len(citations)), min(max(citations), len(citations))
-        def check_h_index(h, citations):
-            counter = 0
-            for cite in citations:
-                if cite >= h:
-                    counter += 1
-            if counter >= h:
-                return True
-            else:
-                return False
-        while left < right:
-            mid = (left + right + 1) // 2
-            if check_h_index(mid, citations):
-                left = mid
-            else:
-                right = mid-1
-        return left
+        # left, right = min(min(citations), len(citations)), min(max(citations), len(citations))
+        # def check_h_index(h, citations):
+        #     counter = 0
+        #     for cite in citations:
+        #         if cite >= h:
+        #             counter += 1
+        #     if counter >= h:
+        #         return True
+        #     else:
+        #         return False
+        # while left < right:
+        #     mid = (left + right + 1) // 2
+        #     if check_h_index(mid, citations):
+        #         left = mid
+        #     else:
+        #         right = mid-1
+        # return left
         
+        bucket = {}
+        for cite in citations:
+            bucket[cite] = bucket.get(cite, 0) +1
+        counter = 0
+        for i in range(max(citations), -1, -1):
+            counter += bucket.get(i, 0)
+            if counter >= i:
+                return i
