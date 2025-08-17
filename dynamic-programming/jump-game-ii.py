255649e57ace7c6ee844d41c1f780d83f1264b4a
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-08-16 22:02:27 -0400

    LC-jump-game-ii [Runtime: 7 ms; Memory: 13.3 MB]

--- dynamic-programming/jump-game-ii.py
+++ dynamic-programming/jump-game-ii.py
@@ -4,11 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        dp = [0] * len(nums) # dp[i]: the minimal jumps needed from i to the end
-        for i in range(len(nums)-2, -1, -1):
-            min_jump = float('inf')
-            for j in range(i+1, min(i+nums[i]+1, len(nums))):
-                min_jump = min(min_jump, dp[j])
-            dp[i] = min_jump + 1
-        return dp[0]
+        # dp = [0] * len(nums) # dp[i]: the minimal jumps needed from i to the end
+        # for i in range(len(nums)-2, -1, -1):
+        #     min_jump = float('inf')
+        #     for j in range(i+1, min(i+nums[i]+1, len(nums))):
+        #         min_jump = min(min_jump, dp[j])
+        #     dp[i] = min_jump + 1
+        # return dp[0]
         
+        # greedy solution
+        jumps = 0
+        farthest = 0
+        cur_end = 0
+        for i in range(len(nums)-1):
+            farthest = max(farthest, i+nums[i])
+            if i == cur_end:
+                jumps += 1
+                cur_end = farthest
+        return jumps
