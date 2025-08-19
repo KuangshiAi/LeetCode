84c627596ca8c1479c70595befdbf1ce5f39c23c
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-08-19 16:49:49 -0400

    LC-minimum-path-sum [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/minimum-path-sum.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def minPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        # dp[i][j]: smallest sum that ends at grid[i][j]
+        m = len(grid)
+        n = len(grid[0])
+        dp = [[0]*n for _ in range(m)]
+        dp[0][0] = grid[0][0]
+        for i in range(m):
+            for j in range(n):
+                if i == 0:
+                    dp[i][j] = dp[i][j-1] + grid[i][j]
+                elif j == 0:
+                    dp[i][j] = dp[i-1][j] + grid[i][j]
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j] = min(dp[i][j-1], dp[i-1][j]) + grid[i][j]
+        return dp[m-1][n-1]
