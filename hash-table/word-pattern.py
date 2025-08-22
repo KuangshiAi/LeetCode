ced2ba0a6936dfd354cec0325192d1783e0c5d94
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-08-22 13:43:27 -0400

    LC-word-pattern [Runtime: N/A; Memory: N/A]

--- hash-table/word-pattern.py
+++ hash-table/word-pattern.py
@@ -9,7 +9,7 @@ class Solution(object):
         hashmap = {}
         for i in range(len(pattern)):
             char = pattern[i]
-            if char not in hashmap.keys():
+            if char not in hashmap.keys() and words[i] not in hashmap.values():
                 hashmap[char] = words[i]
             elif words[i] != hashmap[char]:
                 return False
