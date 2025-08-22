724ab82f40f5185a0ed2fd4b538e5dd8f9c961c9
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-08-22 13:36:07 -0400

    LC-insert-delete-getrandom-o1 [Runtime: N/A; Memory: N/A]

--- array/insert-delete-getrandom-o1.py
+++ array/insert-delete-getrandom-o1.py
@@ -14,6 +14,7 @@ class RandomizedSet(object):
             return False
         self.pos[val] = len(self.arr)
         self.arr.append(val)
+        return True
         
 
     def remove(self, val):
