0724a1d918845116e16d8d1e1fd102f585c553f0
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-08-22 13:42:19 -0400

    LC-word-pattern [Runtime: N/A; Memory: N/A]

---
+++ string/word-pattern.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def wordPattern(self, pattern, s):
+        """
+        :type pattern: str
+        :type s: str
+        :rtype: bool
+        """
+        words = s.split(' ')
+        hashmap = {}
+        for i in range(len(pattern)):
+            char = pattern[i]
+            if char not in hashmap.keys():
+                hashmap[char] = words[i]
+            elif words[i] != hashmap[char]:
+                return False
+        return True
+                
+        
