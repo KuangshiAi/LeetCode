7fa837ea4f9e9909a8dfbe48bb479c4594de93ec
Leo Ai <98941044+KuangshiAi@users.noreply.github.com>
2025-08-29 12:59:49 -0400

    LC-minimum-operations-to-make-character-frequencies-equal [Runtime: N/A; Memory: N/A]

--- counting/minimum-operations-to-make-character-frequencies-equal.py
+++ counting/minimum-operations-to-make-character-frequencies-equal.py
@@ -1,34 +1,24 @@
 class Solution(object):
     def makeStringGood(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        freq = [0]*26
+        freq = [0] * 26
         for char in s:
             freq[ord(char)-ord('a')] += 1
-
-        memo = {}
         def dp(i, deleted, target):
-            key = (i, deleted, target)
-            if key in memo:
-                return memo[key]
             if i == 26:
                 return 0
             x = freq[i]
-            if x == target:
-                res = dp(i+1, 0, target)
+            if x == target or x==0:
+                return dp(i+1, 0, target)
             elif x > target:
-                res = dp(i+1, x-target, target) + x-target
+                return dp(i+1, x-target, target) + x - target
             else:
-                insert = dp(i+1, 0, target) + target - x
+                goal = target - x
+                insert = dp(i+1, 0, target) + goal
                 delete = dp(i+1, x, target) + x
-                change = dp(i+1, 0, target) + target - x - min(deleted, target - x)
-                res = min(insert,delete,change)
-            memo[key] = res
-            return res
-        
+                change = dp(i+1, 0, target) + goal - min(goal, deleted)
+                return min(insert, delete, change)
+
         mini = float('inf')
-        for target in range(max(freq)+1):
+        for target in range(max(freq) + 1):
             mini = min(mini, dp(0,0,target))
         return mini
